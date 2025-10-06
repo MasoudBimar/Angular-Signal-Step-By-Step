c9b031c93efa43a1430e4a810e8e56dbe2e1bd18
Masoud Bimmar <masoudbimar@gmail.com>
2025-10-06 23:57:27 +0200

    Signal Output/Input

--- 07-signal-apis/src/app/components/currency-converter/currency-converter.component.ts
+++ 07-signal-apis/src/app/components/currency-converter/currency-converter.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, EventEmitter, Input, input, OnChanges, output, Output, signal } from '@angular/core';
 import { RATES } from './rates';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject, interval, map, startWith, Subject, switchMap, takeUntil } from 'rxjs';
+import { BehaviorSubject, EMPTY, interval, map, startWith, Subject, switchMap, takeUntil } from 'rxjs';
 import { outputFromObservable, takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -13,22 +13,25 @@ import { outputFromObservable, takeUntilDestroyed } from '@angular/core/rxjs-int
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CurrencyConverterComponent {
-  readonly manualRefresh$ = new BehaviorSubject<void>(undefined);
+  readonly manualRefresh$ = new BehaviorSubject<void>(undefined); // we need initial value for switchMap to work
 
   private readonly stop$ = new Subject<void>();
   stopRefresh() {
     this.stop$.next();
   }
 
+
+  // refresh every 5 seconds or user clicks the refresh button
   readonly refreshRequired$ = this.manualRefresh$.pipe(
-    switchMap(() => interval(2000).pipe(startWith(0))),
+    switchMap(() => interval(5000).pipe(startWith(0))),
     map(() => {}),
     takeUntilDestroyed(), 
     takeUntil(this.stop$)
   );
 
 
-  readonly refreshRequired = outputFromObservable(this.refreshRequired$);
+  // readonly refreshRequired = output<void>();
+  readonly refreshRequired = outputFromObservable(this.refreshRequired$); // integrat output with observable
   
   readonly amount = input.required<number>();
   readonly currency = input.required<string>();
