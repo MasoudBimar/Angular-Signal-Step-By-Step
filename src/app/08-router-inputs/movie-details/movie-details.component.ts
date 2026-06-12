00b48a1cde47f244d18c16f7098a87b749bf04af
MasoudBimar <MasoudBimar@gmail.com>
2026-06-12 14:54:38 +0200

    Router inputs example using the new angular signal api plus rout param as input added

--- src/app/08-router-inputs/movie-details/movie-details.component.ts
+++ src/app/08-router-inputs/movie-details/movie-details.component.ts
@@ -1,40 +1,31 @@
 import { Component, computed, inject, input, numberAttribute } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { map } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MoviesService } from '../services/movies.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterModule, CommonModule, MatCardModule, MatAnchor],
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.scss'
+  styleUrl: './movie-details.component.scss',
 })
 export default class MovieDetailsComponent {
-  // readonly route = inject(ActivatedRoute);
   readonly moviesService = inject(MoviesService);
 
   readonly id = input.required({ transform: numberAttribute });
 
-  // readonly id$ = this.route.params.pipe(
-  //   map(params => Number(params['id']))
-  // );
+  readonly movie = computed(() => this.moviesService.movies().find((mv) => mv.id === this.id()));
 
-  // readonly movie$ = this.id$.pipe(
-  //   map(id => this.moviesService.movies()
-  //             .find(movie => movie.id === id)!)
-  // );
+  readonly poster = computed(() => `movies/${this.movie()?.posterImage ?? ''}`);
 
-  readonly movie = computed(() =>
-    this.moviesService.movies()
-      .find(mv => mv.id === this.id()));
+  renderImagePlaceholder(event: Event): void {
+    const image = event.target as HTMLImageElement;
 
-  // readonly poster$ = this.movie$.pipe(
-  //   map(movie => `movies/${movie.posterImage}`)
-  // );
-
-  readonly poster = computed(
-    () => `movies/${this.movie()?.posterImage ?? ''}`);
-
+    image.onerror = null; // prevents an infinite loop if placeholder also fails
+    image.src = './images/image-placeholder.jpg';
+  }
 }
