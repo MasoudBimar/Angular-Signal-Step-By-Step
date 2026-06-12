bb0321928d213ad82b040a3cf379dcaa590b92de
MasoudBimar <MasoudBimar@gmail.com>
2026-06-12 14:55:31 +0200

    migrate to tailwindcss and new style updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation(),withComponentInputBinding()),// functions starts with "with" are called router configuration functions, 
+    // they configure the router with specific features, in this case we are configuring the router to use hash-based navigation 
+    // and to support component input binding for route parameters.
     provideZonelessChangeDetection(),
   ]
 };
